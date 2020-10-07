4a7fc0e3c6c4a5f6fffa4c3035370252dec600dc
Nishita <goyalnishita07@gmail.com>
2020-10-07 12:14:45 +0530

    Localization

--- app/src/main/java/com/something/myapplication/activity/settingsActivity/SettingsActivity.java
+++ app/src/main/java/com/something/myapplication/activity/settingsActivity/SettingsActivity.java
@@ -1,7 +1,8 @@
 package com.something.myapplication.activity.settingsActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
+import androidx.preference.ListPreference;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -11,17 +12,20 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
 import android.widget.Toast;
 
 import com.something.myapplication.R;
 import java.util.Locale;
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Context mContext;
     private Activity mActivity;
-    Resources resources;
+//public static final String KEY_PREF_LANGUAGE = "pref_key_language";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,26 +36,61 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
         mContext = getApplicationContext();
         mActivity = SettingsActivity.this;
+//        Toast.makeText(mContext, "English language chosen!", Toast.LENGTH_LONG).show();
+
+
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+    public static class MyPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-         public static final String KEY_PREF_LANGUAGE = "pref_key_language";
+        public static final String KEY_PREF_LANGUAGE = "pref_key_language";
+
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings);
+            //Toast.makeText(getContext(), "English language chosen!", Toast.LENGTH_LONG).show();
         }
+
 
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
             switch (key) {
                 case KEY_PREF_LANGUAGE:
-                    LocaleHelper.setLocale(getContext(), PreferenceManager.getDefaultSharedPreferences(getContext()).getString(key, ""));
+                    LocaleHelper.setLocale(getContext(),PreferenceManager.getDefaultSharedPreferences(getContext()).getString(key, ""));
                     getActivity().recreate(); // necessary here because this Activity is currently running and thus a recreate() in onResume() would be too late
                     break;
             }
+//            if (key.equals(KEY_PREF_LANGUAGE)) {
+//                Preference connectionPref = findPreference(key);
+//                connectionPref.setSummary(sharedPreferences.getString(key, ""));
+//                updateLanguage(getContext(), sharedPreferences.getString(key, ""));
+//            }
         }
+
+//        private void updateLanguage(Context context, String selectedLanguage) {
+//            if (!"".equals(selectedLanguage)) {
+//                if ("English".equals(selectedLanguage)) {
+//                    selectedLanguage = "en";
+//                    LocaleHelper.setLocale(context, selectedLanguage);
+//                    getActivity().recreate();
+//                    Toast.makeText(context, "English language chosen!", Toast.LENGTH_LONG).show();
+//                }
+//                else if ("Hindi".equals(selectedLanguage)) {
+//                    selectedLanguage = "hi-rIN";
+//                    LocaleHelper.setLocale(context, selectedLanguage);
+//                    getActivity().recreate();
+//                    Toast.makeText(context, "Hindi language chosen!", Toast.LENGTH_LONG).show();
+//
+//                }
+//                Locale locale = new Locale(selectedLanguage);
+//                Locale.setDefault(locale);
+//                Configuration config = new Configuration();
+//                config.locale = locale;
+//                context.getResources().updateConfiguration(config, null);
+         //   }
+
+
         @Override
         public void onResume() {
             super.onResume();
@@ -63,6 +102,7 @@ public class SettingsActivity extends AppCompatActivity {
         public void onPause() {
             super.onPause();
             getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+
         }
     }
 }
